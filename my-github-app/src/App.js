e32dc3e87cc6cb3e111577124a4aebae6a569d64
Katrina_Dierking <kalynndier@gmail.com>
2019-11-03 21:42:10 -0500

    user card created

--- my-github-app/src/App.js
+++ my-github-app/src/App.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import axios from 'axios';
+import Users from './Components/Users';
 
 
 
+class App extends React.Component {
+  constructor() {
+    super(); 
+    this.state = {
+      users: [], 
+      followers: []
+    }
+  }
 
-function App() {
+  componentDidMount() {
+    axios 
+      .get ("https://api.github.com/users/Katrina-Dierking")
+      .then(res => this.setState({users:res.data}))
+  
+    axios 
+      .get ("https://api.github.com/users/Katrina-Dierking/followers")
+      .then(res => this.setState({followers:res.data}))
+  }  
+
+render () {
   return (
+    <>
     <div className = "App">
-      <h1>Katrina Dierking's GitHub Profile</h1>
-      <br></br>
-      <h1>My Followers</h1>
+      <div className = "app-header">
+        <h1>Katrina Dierking's GitHub Profile</h1>
+        <br></br>
+        <h1>My Followers</h1>
+      </div>
+
+      <Users users = {this.state.users}
+             followers={this.state.followers}/>
+
+      {/* {this.state.followers.map(follower => {
+        return <Followers login = {follower.login}/>
+      })}  */}
       
     </div>
-  )
+  </>
+  );
+}
 }
 
 
